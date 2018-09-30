8a36648cad6357e08cff23d41f043bf6b97f6ad8
eneko98 <valeroeneko@gmail.com>
2018-09-30 12:08:18 +0200

    Ventana

--- ProyectoProg3/src/Ventana.java
+++ ProyectoProg3/src/Ventana.java
@@ -1,9 +1,39 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
-public class Ventana {
+import javax.swing.JFrame;
+import javax.swing.*;
 
-	String titulo;
-	Dimension dimension = new Dimension(600, 900);
+
+public class Ventana extends JFrame {
+
+	String titulo = "Ventana Principal";
+	Dimension dimension = new Dimension(900, 900);
 	
 	
+	public Ventana(){
+		setTitle(titulo);
+		setResizable(false);
+		setSize(dimension.width, dimension.height);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		
+		
+		JButton boton1 = new JButton("Reproductor de musica");
+		boton1.setBounds(150, 100, 120, 60);
+		
+		JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setVisible(true);
+		panelPrincipal.add(boton1);
+		
+		getContentPane().add(panelPrincipal);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Ventana v = new Ventana();
+		v.setVisible(true);
+	}
 }
+
