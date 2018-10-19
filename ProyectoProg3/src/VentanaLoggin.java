f4451469d253c8d7d3561c468fc290b172cbfcd1
DiezAndino.Daniel <33398625+Drakensw@users.noreply.github.com>
2018-10-19 16:34:46 +0200

    Interfaz

---
+++ ProyectoProg3/src/VentanaLoggin.java
@@ -0,0 +1,79 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaLoggin {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaLoggin window = new VentanaLoggin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaLoggin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 347, 179);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNombre.setBounds(29, 13, 94, 30);
+		frame.getContentPane().add(lblNombre);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblContrasea.setBounds(29, 56, 94, 24);
+		frame.getContentPane().add(lblContrasea);
+		
+		textField = new JTextField();
+		textField.setBounds(156, 18, 116, 22);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(156, 58, 116, 24);
+		frame.getContentPane().add(passwordField);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAceptar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnAceptar.setBounds(106, 93, 97, 25);
+		btnAceptar.setFocusPainted(false);
+		frame.getContentPane().add(btnAceptar);
+	}
+}
