89865f7fa8aacc102d865430f7e3cef3fe6493ef
DiezAndino.Daniel <33398625+Drakensw@users.noreply.github.com>
2018-11-11 11:44:46 +0100

    Añadicion de nuevas librerias. Creacion de los botones de control de audio y creacion de MediaPlayer en VentanaReproductor sin exito

--- ProyectoProg3/src/VentanaReproductor.java
+++ ProyectoProg3/src/VentanaReproductor.java
@@ -1,43 +1,50 @@
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
+import java.util.Date;
 
-import javax.media.Manager;
-import javax.media.Player;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextPane;
+
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.windows.Win32FullScreenStrategy;
+
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+
+
+import javax.swing.BoxLayout;
 
 public class VentanaReproductor extends JFrame {
 	
 	String titulo = "Ventana Reproductor";
 	Dimension dimension = new Dimension(900, 900);
 	
-	URL urlArchivoMusica;
-	Player reproductor;
+	// Atributos de VLCj
+	
+	//EmbeddedMediaPlayerComponent mediacom = new EmbeddedMediaPlayerComponent();   
 	
 	
 	public VentanaReproductor() {
-		try {
-			urlArchivoMusica = new URL("file:/C:/Users/Diazl//Desktop/EminemTheRinger.mp3");
-			reproductor = Manager.createRealizedPlayer(urlArchivoMusica);
-			Component controles = reproductor.getControlPanelComponent();
+		
+		//Intento creacion reproductor
+		
+			String mediaPath = "C:\\Users\\Diazl\\Desktop\\Dani\\Universidad de Deusto\\3º-Simestre 1\\Programacion\\Proyecto Prog3"; 	
+		   // mplayer.setAudioTrack(1);
 			
-			if(controles != null) {
-				getContentPane().add(controles);
-				controles.setLocation(12,191);
-				controles.setSize(283,25);
-			}
-		} catch (Exception e) {
-			System.out.println("Se ha encontrado un error al trata de reproducir el archivo");
-		}
-	
+			MediaPlayerFactory mpf = new MediaPlayerFactory();
+			EmbeddedMediaPlayerComponent emp = (EmbeddedMediaPlayerComponent) mpf.newEmbeddedMediaPlayer();
+			
+			
+		
 			setTitle(titulo);
 			setResizable(false);
 			setBounds(100, 100, 652, 406);
@@ -47,7 +54,9 @@ public class VentanaReproductor extends JFrame {
 			JPanel panelImagenCaratula = new JPanel();
 			panelImagenCaratula.setBackground(Color.WHITE);
 			panelImagenCaratula.setBounds(12, 13, 283, 191);
+			
 			getContentPane().add(panelImagenCaratula);
+					
 			
 			JTextPane textoLirica = new JTextPane();
 			textoLirica.setEditable(false);
@@ -60,6 +69,10 @@ public class VentanaReproductor extends JFrame {
 			getContentPane().add(panelBotonesLirica);
 			
 			JButton botonAñadirLirica = new JButton("Añadir Lirica");
+			botonAñadirLirica.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
 			botonAñadirLirica.setFocusPainted(false);
 			panelBotonesLirica.add(botonAñadirLirica);
 			
@@ -73,14 +86,69 @@ public class VentanaReproductor extends JFrame {
 				}
 			});
 			botonCancelar.setFont(new Font("Tahoma", Font.PLAIN, 15));
-			botonCancelar.setBounds(12, 278, 102, 39);
+			botonCancelar.setBounds(12, 297, 102, 39);
 			botonCancelar.setFocusPainted(false);
 			getContentPane().add(botonCancelar);
+			
+			JPanel panel = new JPanel();
+			panel.setBackground(Color.WHITE);
+			panel.setBounds(22, 228, 249, 56);
+			getContentPane().add(panel);
+			panel.setLayout(null);
+			
+			JButton botonSiguiente = new JButton("");
+			botonSiguiente.setBackground(Color.WHITE);
+			botonSiguiente.setIcon(new ImageIcon(VentanaReproductor.class.getResource("/imagenes/Next.png")));
+			botonSiguiente.setBounds(186, 0, 63, 56);
+			botonSiguiente.setFocusPainted(false);
+			botonSiguiente.addActionListener( new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+
+					
+				}
+			});
+			panel.add(botonSiguiente);
+			
+			JButton botonPause = new JButton("");
+			botonPause.setBackground(Color.WHITE);
+			botonPause.setIcon(new ImageIcon(VentanaReproductor.class.getResource("/imagenes/Pause.png")));
+			botonPause.setBounds(0, 0, 63, 56);
+			botonPause.setFocusPainted(false);
+			panel.add(botonPause);
+			
+			JButton botonPlay = new JButton("");
+			botonPlay.setBackground(Color.WHITE);
+			botonPlay.setIcon(new ImageIcon(VentanaReproductor.class.getResource("/imagenes/Play.png")));
+			botonPlay.setBounds(62, 0, 63, 56);
+			botonPlay.setFocusPainted(false);
+			botonPlay.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					//mplayer.playMedia(mediaPath);
+				}
+			});
+			panel.add(botonPlay);
+			
+			JButton botonAnterior = new JButton("");
+			botonAnterior.setBackground(Color.WHITE);
+			botonAnterior.setIcon(new ImageIcon(VentanaReproductor.class.getResource("/imagenes/Back.png")));
+			botonAnterior.setBounds(124, 0, 63, 56);
+			botonAnterior.setFocusPainted(false);
+			panel.add(botonAnterior);
+			
+			JPanel barraReproductor = new JPanel();
+			barraReproductor.setBounds(12, 208, 283, 14);
+			getContentPane().add(barraReproductor);
 		}
+	
+	
 	
 	public static void main(String[] args) {
 		VentanaReproductor vr = new VentanaReproductor();
 		vr.setVisible(true);
+		MediaPlayer mplayer;
+		
 	}
-			
 }
